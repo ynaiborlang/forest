b846a91e5801e1c2daad5edecd187e80f6cab055
ynaiborlang <ynaiborlang@gmail.com>
2023-04-07 15:51:21 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "assets/AssetManifest.json": "d05aa108768560deef417e1cd73d8ebd",
-"assets/assets/forest.riv": "3d52ce745a8dc8e47b9bb99f6cb58798",
+"assets/assets/forest.riv": "0e134033a9f7c42768dbd4d0f0bd6035",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/NOTICES": "c5e2ae54d0abccf23bea202ffaeff0a7",
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "06b65f949e6b98731e11d20284b1ec4e",
-"/": "06b65f949e6b98731e11d20284b1ec4e",
-"main.dart.js": "451e92220afd5ca31d75a8851896c357",
+"index.html": "090b941f45f2fb91cb418c464d1c8aed",
+"/": "090b941f45f2fb91cb418c464d1c8aed",
+"main.dart.js": "ceeeb67beb72434a429999d49dd59058",
 "manifest.json": "647bf3bbef64fc070412f387f43f463e",
 "version.json": "825fb0e9f86807efed67feed98568c3e"
 };
