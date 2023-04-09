b76283d3a6adce060b1b94d17b37ca757ef03cb8
ynaiborlang <ynaiborlang@gmail.com>
2023-04-09 12:57:13 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,8 +20,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "36a1218b994cfe4172dcdc490f4ca206",
-"/": "36a1218b994cfe4172dcdc490f4ca206",
+"index.html": "571f2793e5b0566cd327a975e88adb6c",
+"/": "571f2793e5b0566cd327a975e88adb6c",
 "main.dart.js": "b5b98c7b4dbe30a2dc946a9a7129eda2",
 "manifest.json": "647bf3bbef64fc070412f387f43f463e",
 "version.json": "825fb0e9f86807efed67feed98568c3e"
